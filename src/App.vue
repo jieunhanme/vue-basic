9aab0388c068068ac1b317d3a4c0db57ea26d35a
jieun.han <jieun.han@brique.co.kr>
2023-01-27 12:23:15 +0900

    feat: Vue 기본 데이터 흐름 공부

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,164 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <modal-detail
+    :isModalActive="isModalActive"
+    :changeModalState="changeModalState"
+    :post="getContent"
+    @editCountFromChild="editCount"
+  />
+
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu">{{ menu }}</a>
+  </div>
+  <section>
+    <div
+      v-for="(post, i) in posts"
+      :key="post.id"
+      class="card"
+      @click="changeModalState(i)"
+    >
+      <div class="image">
+        <img :src="post.image" />
+      </div>
+      <div class="description">
+        <h4>{{ post.title }}</h4>
+
+        <div class="optional">
+          <span>🎀 {{ post.count }}</span>
+          <div class="price">
+            <span v-if="post.count >= 5" class="badge">인기</span>
+            <span>{{ post.price.toLocaleString("ko-KR") }}억</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "./data/post";
+import ModalDetail from "./components/ModalDetail.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      isModalActive: false,
+      activeCard: -1,
+      posts: data,
+      menus: ["HOME", "SHOP", "ABOUT"],
+    };
+  },
+  methods: {
+    editCount() {
+      return this.posts[this.activeCard].count++;
+    },
+    changeModalState(i) {
+      this.activeCard = i;
+      this.isModalActive = !this.isModalActive;
+    },
+  },
+  computed: {
+    getContent() {
+      if (this.activeCard < 0 || !this.isModalActive) return;
+      return this.posts[this.activeCard];
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    ModalDetail,
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: rgba(70, 139, 61, 0.87);
+  padding: 15px;
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: center;
+  grid-gap: 10px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  border: 2px dashed rgba(255, 255, 255, 0.5);
+  border-radius: 5px;
+}
+
+.menu a:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.card {
+  height: 230px;
+  width: 280px;
+  border: 1px solid rgba(128, 128, 128, 0.164);
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.card:hover {
+  box-shadow: 5px 5px 3px rgba(102, 102, 102, 0.096);
+}
+
+.card img {
+  width: 100%;
+}
+.card > .description {
+  margin: 10px 8px;
+}
+
+.description h4 {
+  text-align: left;
+  margin: 0;
+  min-height: 40px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; /* 라인수 */
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+}
+
+.description > .price {
+  text-align: right;
+  margin-bottom: 5px;
+}
+
+.badge {
+  color: red;
+  font-size: small;
+  vertical-align: top;
+  margin-right: 5px;
+}
+
+.optional {
+  display: flex;
+  justify-content: space-between;
+}
+
+section {
+  padding: 20px 0;
+  display: grid;
+  justify-content: center;
+  grid-gap: 20px;
+  grid-template-columns: 280px 280px 280px;
 }
 </style>
---
+++ src/components/ModalDetail.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="black-bg" v-if="isModalActive">
+    <div class="white-bg">
+      <div class="modal-header">
+        <h4>상세페이지</h4>
+        <button @click="changeModalState">X</button>
+      </div>
+      <div class="modal-detail">
+        <img :src="post.image" />
+        <p>{{ post.title }}</p>
+        <p>{{ post.content }}</p>
+        <button @click="handleAddFavorite">Add Favorite</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalDetail",
+  props: {
+    isModalActive: { type: Boolean, default: false },
+    changeModalState: { type: Function },
+    post: { type: Object, default: null },
+  },
+  methods: {
+    handleAddFavorite() {
+      this.$emit("editCountFromChild");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+  display: flex;
+}
+.white-bg {
+  width: 500px;
+  background-color: white;
+  border-radius: 8px;
+  margin: auto;
+}
+
+.modal-header {
+  display: flex;
+  width: 100%;
+  padding: 20px 20px 15px 20px;
+  border-bottom: 1px solid rgba(128, 128, 128, 0.164);
+  justify-content: space-between;
+}
+
+.modal-header h4 {
+  margin: 0;
+}
+.modal-detail {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 300px;
+  padding: 20px 20px;
+}
+
+.modal-detail p:first-of-type {
+  font-weight: bold;
+}
+.modal-detail p:last-of-type {
+  margin-top: 0;
+}
+
+.modal-detail > img {
+  width: 100%;
+  border-radius: 4px;
+}
+</style>
